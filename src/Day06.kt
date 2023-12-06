cf9a1eb838548d0a2533bb98e03a53a3a30d1933
Radostin Marinov <radostinmar21@gmail.com>
2023-12-06 07:18:51 +0200

    Add day 6 solution

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,3 +1,22 @@
-object Day06 : Day(isTest = true) {
+object Day06 : Day() {
 
+    override fun part1(): Number {
+        val times = lines[0].substringAfter("Time:").trim().split("\\s+".toRegex()).toInts()
+        val records = lines[1].substringAfter("Distance:").trim().split("\\s+".toRegex()).toInts()
+        return times.zip(records).map { (time, record) ->
+            (0..time).count {holdTime ->
+                val distance = holdTime * (time - holdTime)
+                distance > record
+            }
+        }.product
+    }
+
+    override fun part2(): Number {
+        val time = lines[0].substringAfter("Time:").replace(" ", "").toLong()
+        val record = lines[1].substringAfter("Distance:").replace(" ", "").toLong()
+        return (0..time).count { holdTime ->
+            val distance = holdTime * (time - holdTime)
+            distance > record
+        }
+    }
 }
