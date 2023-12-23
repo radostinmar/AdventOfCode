b6837f465dbc387bf01bedf43e8d828d1af639ce
Radostin Marinov <radostinmar21@gmail.com>
2023-12-23 10:53:54 +0200

    Add better day 23 solution

--- src/y2023/Day23.kt
+++ src/y2023/Day23.kt
@@ -8,21 +8,19 @@ import util.pTo
 
 object Day23 : Day(isTest = false) {
 
-    override fun part1(): Any {
+    override fun part1(): Any = solve(withSlopes = true)
+
+    override fun part2(): Any = solve(withSlopes = false)
+
+    private fun solve(withSlopes: Boolean): Int {
         val start = 0 pTo lines.first().indexOfFirst { it == '.' }
         val end = lines.lastIndex pTo lines.last().indexOfFirst { it == '.' }
-        return findMax(start, end, emptyList())
+        val intersections = (findIntersections(withSlopes) + start + end).toSet()
+        val graph = intersections.associateWith { findClosest(it, intersections, withSlopes) }
+        return findMax(start, end, emptySet(), graph)
     }
 
-    override fun part2(): Any {
-        val start = 0 pTo lines.first().indexOfFirst { it == '.' }
-        val end = lines.lastIndex pTo lines.last().indexOfFirst { it == '.' }
-        val intersections = (findIntersections() + start + end).toSet()
-        val graph = intersections.associateWith { findClosest(it, intersections) }
-        return findMax2(start, end, emptySet(), graph)
-    }
-
-    private fun findMax2(
+    private fun findMax(
         start: Point,
         end: Point,
         visited: Set<Point>,
@@ -32,20 +30,20 @@ object Day23 : Day(isTest = false) {
             return 0
         }
         val neighbours = graph.getValue(start).filter { it.key !in visited }
-        if(neighbours.isEmpty()) {
+        if (neighbours.isEmpty()) {
             return Int.MIN_VALUE
         }
         return neighbours.maxOf { entry ->
-            entry.value + findMax2(entry.key, end, visited + start, graph)
+            entry.value + findMax(entry.key, end, visited + start, graph)
         }
     }
 
-    private fun findIntersections(): List<Point> = lines.flatMapIndexed { row, s ->
+    private fun findIntersections(withSlopes: Boolean): List<Point> = lines.flatMapIndexed { row, s ->
         s.mapIndexedNotNull { col, c ->
             if (c == '#') {
                 null
             } else {
-                val neighbours = getNeighbours(row pTo col).size
+                val neighbours = getNeighbours(row pTo col, withSlopes).size
                 if (neighbours > 2) {
                     row pTo col
                 } else {
@@ -55,29 +53,39 @@ object Day23 : Day(isTest = false) {
         }
     }
 
-    private fun findClosest(start: Point, intersections: Set<Point>): Map<Point, Int> {
-        val neighbours = getNeighbours(start)
-        return neighbours.associate {
-            findNextIntersection(it, setOf(start), intersections, 1)
-        }
+    private fun findClosest(start: Point, intersections: Set<Point>, withSlopes: Boolean): Map<Point, Int> {
+        val neighbours = getNeighbours(start, withSlopes)
+        return neighbours.mapNotNull {
+            findNextIntersection(it, setOf(start), intersections, 1, withSlopes)
+        }.toMap()
     }
 
     private fun findNextIntersection(
         start: Point,
         visited: Set<Point>,
         intersections: Set<Point>,
-        distance: Int
-    ): Pair<Point, Int> {
-        val next = getNeighbours(start).first { it !in visited }
+        distance: Int,
+        withSlopes: Boolean
+    ): Pair<Point, Int>? {
+        val next = getNeighbours(start, withSlopes).find { it !in visited }
+        next ?: return null
         return if (next in intersections) {
             next to (distance + 1)
         } else {
-            findNextIntersection(next, visited + start, intersections, distance + 1)
+            findNextIntersection(next, visited + start, intersections, distance + 1, withSlopes)
         }
     }
 
-    private fun getNeighbours(point: Point): List<Point> =
+    private fun getNeighbours(point: Point, withSlopes: Boolean): List<Point> =
         Direction.entries.mapNotNull {
+            if (withSlopes) {
+                when (lines[point]) {
+                    '>' -> if (it != Direction.EAST) return@mapNotNull null
+                    '<' -> if (it != Direction.WEST) return@mapNotNull null
+                    '^' -> if (it != Direction.NORTH) return@mapNotNull null
+                    'v' -> if (it != Direction.SOUTH) return@mapNotNull null
+                }
+            }
             val new = point + it
             if (new.isInBounds(lines.lastIndex, lines.first().lastIndex) && lines[new] != '#') {
                 new
@@ -85,32 +93,4 @@ object Day23 : Day(isTest = false) {
                 null
             }
         }
-
-    private fun findMax(start: Point, end: Point, visited: List<Point>): Int {
-        if (start == end) {
-            return 0
-        }
-        val neighbours = when (lines[start]) {
-            '.' -> Direction.entries.mapNotNull {
-                val newPoint = start + it
-                if (newPoint.x > 0) {
-                    newPoint
-                } else {
-                    null
-                }
-            }
-
-            '>' -> listOf(start + Direction.EAST)
-            '<' -> listOf(start + Direction.WEST)
-            '^' -> listOf(start + Direction.NORTH)
-            'v' -> listOf(start + Direction.SOUTH)
-            else -> emptyList()
-        }.filter { lines[it] != '#' && it !in visited }
-        if (neighbours.isEmpty()) {
-            return Int.MIN_VALUE
-        }
-        return 1 + neighbours.maxOf {
-            findMax(it, end, visited + start)
-        }
-    }
 }
