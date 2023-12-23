a624675e10fcdad2ed75ed14be62c4f9ab628453
Radostin Marinov <radostinmar21@gmail.com>
2023-12-23 20:26:22 +0200

    Add 2022 day 16 part 1 solution

--- src/y2022/Day16.kt
+++ src/y2022/Day16.kt
@@ -1,87 +1,91 @@
 package y2022
 
 import Day
-import java.util.*
 
 object Day16 : Day(year = 2022, isTest = true) {
 
-    private fun findBest(start: Valve, valves: Map<String, Valve>): Int {
-        val queue = PriorityQueue<State>()
-        queue.add(State(start, emptyList(), 1, 0))
-        var max = 0
-        while (queue.isNotEmpty()) {
-            val current = queue.poll()
-            if (current.minute == 30) {
-                if (current.pressure > max) {
-                    println(current)
-                    max = current.pressure
-                }
-                continue
-            }
-            val released = current.open.sumOf(Valve::flowRate)
-            if (current.valve.flowRate != 0 && current.valve !in current.open) {
-                queue.add(
-                    State(
-                        valve = current.valve,
-                        open = current.open + current.valve,
-                        minute = current.minute + 1,
-                        pressure = current.pressure + released
-                    )
-                )
-            }
-            current.valve.leadsTo.forEach {
-                val newValve = valves.getValue(it)
-                queue.add(
-                    State(
-                        valve = newValve,
-                        open = current.open,
-                        minute = current.minute + 1,
-                        pressure = current.pressure + released
-                    )
-                )
-            }
+    private data class State(
+        val current: Valve,
+        val open: Set<Valve>,
+        val minutes: Int
+    )
+
+    private val cache = mutableMapOf<State, Int>()
+
+    private fun findBest(
+        state: State,
+        shortestPaths: Map<Valve, Map<Valve, Int>>,
+        limit: Int
+    ): Int {
+        if (state.minutes == limit) {
+            return 0
         }
-        return max
+        if (state.minutes > limit) {
+            return Int.MIN_VALUE
+        }
+        cache[state]?.let { return it }
+        return shortestPaths.getValue(state.current).maxOf {
+            val timeToReachAndOpen = it.value + 1
+            state.open.sumOf(Valve::flowRate) * timeToReachAndOpen + findBest(
+                state = state.copy(
+                    current = it.key,
+                    open = state.open + it.key,
+                    minutes = state.minutes + timeToReachAndOpen
+                ),
+                shortestPaths = shortestPaths,
+                limit = limit
+            )
+        }.also { cache[state] = it }
     }
 
-    data class State(
-        val valve: Valve,
-        val open: List<Valve>,
-        val minute: Int,
-        val pressure: Int
-    ) : Comparable<State> {
-        override fun compareTo(other: State): Int {
-            val openResult = other.open.sumOf(Valve::flowRate).compareTo(this.open.sumOf(Valve::flowRate))
-
-            return if (openResult != 0) {
-                openResult
-            } else {
-                val pressureResult = other.pressure.compareTo(this.pressure)
-                if (pressureResult != 0) {
-                    pressureResult
-                } else {
-                    other.valve.flowRate.compareTo(this.valve.flowRate)
+    private fun floydWarshall(valves: List<Valve>): Map<Valve, Map<String, Int>> {
+        val distances = valves.associateWith { mutableMapOf(it.name to 0).withDefault { 100 } }.toMutableMap()
+        valves.forEach { valve ->
+            valve.leadsTo.forEach { adjacent ->
+                distances.getValue(valve)[adjacent] = 1
+            }
+        }
+        for (k in valves) {
+            for (i in valves) {
+                for (j in valves) {
+                    if (
+                        distances.getValue(i).getValue(j.name) >
+                        distances.getValue(i).getValue(k.name) + distances.getValue(k).getValue(j.name)
+                    ) {
+                        distances.getValue(i)[j.name] =
+                            distances.getValue(i).getValue(k.name) + distances.getValue(k).getValue(j.name)
+                    }
                 }
             }
         }
+        return distances
     }
 
     data class Valve(
+        val name: String,
         val flowRate: Int,
         val leadsTo: List<String>
     )
 
     override fun part1(): Any {
-        val valves = lines.associate {
+        val valves = lines.map {
             val (valveName, flowRateString, valvesString) =
                 "Valve (.+) has flow rate=(.+); tunnels? leads? to valves? (.+)".toRegex()
                     .find(it)!!.groupValues.drop(1)
-            valveName to Valve(
+            Valve(
+                valveName,
                 flowRateString.toInt(),
                 valvesString.split(", ")
             )
         }
-        return findBest(valves.getValue("AA"), valves)
+        val valvesMap = valves.associateBy { it.name }
+        val valvesWithPositiveFlowRate = valves.filter { it.flowRate > 0 }.toSet()
+        val shortestPaths = floydWarshall(valves).mapValues {
+            it.value
+                .mapKeys { entry -> valvesMap.getValue(entry.key) }
+                .filterKeys { key -> key in valvesWithPositiveFlowRate }
+                .filterValues { value -> value != 0 }
+        }
+        return findBest(State(valvesMap.getValue("AA"), emptySet(), 0), shortestPaths, 30)
     }
-
 }
